3d1bbd6722b5615cead6ae2581b3285d818e5d7e
Hari <harivallabha.rangarajan@gmail.com>
2020-08-31 05:26:45 +0530

    Final TO-DO: Add state_update function to fpi_solver

--- src/core.cpp
+++ src/core.cpp
@@ -226,6 +226,8 @@ void fpi_solver(int iter, Point* globaldata, Config configData, double res_old[1
 
         cal_flux_residual(globaldata, numPoints, configData, Gxp, Gxn, Gyp, Gyn, phi_i, phi_k, G_i, G_k,
             result, qtilde_i, qtilde_k, sig_del_x_del_f, sig_del_y_del_f, main_store);
+
+        //TO-DO: Add the State Update Function... 
     }
 }
 
