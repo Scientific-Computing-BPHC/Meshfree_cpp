133282f1018f812b2d004f3092b031ec2df5d283
Hari <harivallabha.rangarajan@gmail.com>
2021-01-17 06:20:46 +0530

    Add Comments for main_cuda.cu

--- src/CUDA/main_cuda.cu
+++ src/CUDA/main_cuda.cu
@@ -1,3 +1,4 @@
+/* External Header Files */
 #include<stdio.h>
 #include<iostream>
 #include<math.h>
@@ -14,6 +15,7 @@
 #include<tuple>
 #include <chrono>
 
+/* Internal Header Files */
 #include "utils.hpp"
 #include "core_cuda.hpp"
 #include "point.hpp"
@@ -27,26 +29,46 @@ typedef std::vector<std::string> vec_str;
 typedef std::vector<double> vec_doub;
 typedef std::vector<long double> vec_ldoub;
 
-bool debug_mode = true;
+bool debug_mode = true; // Logging
 
+/**
+* Driver function for the meshfree solver. Reads the Quadtree File Input, and Calls the test_code function.
+*
+*@param file_name Grid file containing the point distribution
+*@param max_iters Number of iterations for which the solver is run
+*
+*/
 void meshfree_solver(char* file_name, int num_iters);
+
+/**
+* Driver function for the meshfree solver. Reads the Quadtree File Input, and Calls the test_code function.
+*
+*@param file_name Grid file containing the point distribution
+*@param max_iters Number of iterations for which the solver is run
+*
+*/
 void run_code(Point* globaldata, Config configData, double res_old[1], int numPoints, TempqDers* tempdq, int max_iters, int* xpos_conn, int* xneg_conn, int* ypos_conn, int* yneg_conn, \
 	int* connec, double* prim, double* flux_res, double* q, double* dq1, double* dq2, double* max_q, double* min_q, double* prim_old);
+
+/**
+* Driver function for the meshfree solver. Reads the Quadtree File Input, and Calls the test_code function.
+*
+*@param file_name Grid file containing the point distribution
+*@param max_iters Number of iterations for which the solver is run
+*
+*/
 void test_code(Point* globaldata, Config configData, double res_old[1], int numPoints, int max_iters, int* xpos_conn, int* xneg_conn, int* ypos_conn, int* yneg_conn,\
 	int* connec, double* prim, double* flux_res, double* q, double* dq1, double* dq2, double* max_q, double* min_q, double* prim_old);
 
 int main(int argc, char **argv)
 {
-	printf("\nMeshfree AD\n");
+	printf("\nLeast Squares Kinetic Upwind Meshfree Solver (q-LSKUM)\n");
 
-	/* initialize random seed*/
+	/* initialize random seed */
 	srand (time(NULL));
 	arma_rng::set_seed_random();
 
-	meshfree_solver(argv[1], std::stoi(argv[2])); //Warning: Casting from char to int loses precision
-	//Gotta see if maintaining a global array id efficient or if passing around by reference is efficient
-	//For all we know, maintaining a global data structure instead of passing it around might be more efficient
-
+	meshfree_solver(argv[1], std::stoi(argv[2])); 
 	cout<<"\n Max Iters: "<<std::stoi(argv[2])<<endl;
 }
 
@@ -57,17 +79,12 @@ void meshfree_solver(char* file_name, int max_iters)
 	configData.setConfig();
 	if (debug_mode)
 		configData.printConfig();
-
 	std::string format = configData.format.type;
-	if (debug_mode)
-		cout<<"\nFormat: "<<format<<endl;
-
 	cout<<"\nFilename: "<<file_name<<endl;
-	//cout<<"hi"<<endl;
 
+	/* Read in numPoints from the point ditribution file */
 	int numPoints = 0;
 	std::fstream datafile(file_name, ios::in);
-
 	if(datafile.is_open())
 	{
 		std::string temp;
@@ -76,6 +93,7 @@ void meshfree_solver(char* file_name, int max_iters)
 		num >> numPoints;	
 	}
 
+	/* Read datafile as a string, split using regex, and then convert to double/int/short. A bit tacky, but this parse method isn't essential for our benchmarking */
 	std::string temp;
 	std::string* new_file = new std::string[numPoints];
 	if(datafile.is_open())
@@ -86,22 +104,15 @@ void meshfree_solver(char* file_name, int max_iters)
 		}
 	}
 	datafile.close();
-	
-	//cout<<new_file[1][0]<<endl;
 	cout<<"\nNo. of points: "<<numPoints<<endl;
-
 	std::regex ws_re("\\s+"); 
 	std::vector<vec_str> result;
-	for(int i=0; i<numPoints; i++)  // This might include the first line as well so, you need to store that separately or just throw the 1st line away (the one that contains the file length)
-									// There are 48738 lines apart from that
+	for(int i=0; i<numPoints; i++)
 	{ 
     	std::vector<std::string> temp{std::sregex_token_iterator(new_file[i].begin(), new_file[i].end(), ws_re, -1), {}};
     	result.push_back(temp);
 	}
-	
-	// Free up the space taken by new_file
-	delete[] new_file;
-
+	delete[] new_file; // Free up the space taken up by new_file
 	std::vector<vec_doub> result_doub;
 	for(int j=0; j<numPoints; j++)
     {
@@ -111,9 +122,9 @@ void meshfree_solver(char* file_name, int max_iters)
 		result_doub.push_back(temp);
 
 	}
-
 	std::vector<vec_str>().swap(result); // Free up the space taken up by result
 
+	/* Allocate memory from the heap to store the point data */
 	Point* globaldata = new Point[numPoints];
 	int* xpos_conn = new int[numPoints*20];
 	int* xneg_conn = new int[numPoints*20];
@@ -134,97 +145,74 @@ void meshfree_solver(char* file_name, int max_iters)
 
 	double defprimal[4];
 	getInitialPrimitive(configData, defprimal);
-	//printPrimal(defprimal);
 
 	cout<<"\n-----Start Read-----\n"; // readFile function in Julia
 
 	//cout<<result_doub[0][0]<<endl;
 
 
-/* Read File Legacy */
+	/* Read File Legacy */
+	#if 0
+		for(int idx=0; idx<numPoints; idx++)
+		{
+			int connectivity[20] = {0};
+			for(int iter=8; iter<result_doub[idx].size(); iter++)
+			{
+				connectivity[iter-8] = result_doub[idx][iter];
+			}
+			/* Assign values to each of the elements of global data and ensure the type specifications are adhered to */
+			globaldata[idx].localID = idx;
+			globaldata[idx].x = result_doub[idx][0];
+			globaldata[idx].y = result_doub[idx][1];
+			globaldata[idx].left = (int)result_doub[idx][2];
+			globaldata[idx].right = (int)result_doub[idx][3];
+			globaldata[idx].flag_1 = (int)result_doub[idx][4];
+			globaldata[idx].flag_2 = (int)result_doub[idx][5];
+			globaldata[idx].short_distance = result_doub[idx][6];
+			globaldata[idx].nbhs = (int)result_doub[idx][7];
+			for(int i=0; i<20; i++)
+			{
+				connec[idx*20 +i] = connectivity[i];
+			}
+			globaldata[idx].nx = 0.0;
+			globaldata[idx].ny = 0.0;
+			for(int i=0; i<4; i++)
+			{
+				prim[idx*4 + i] = defprimal[i];
+				flux_res[idx*4 + i] = 0.0;
+				q[idx*4 + i] = 0.0;
+				dq1[idx*4 + i] = 0.0;
+				dq2[idx*4 + i] = 0.0;
+				max_q[idx*4 + i] = 0.0;
+				min_q[idx*4 + i] = 0.0;
+				prim_old[idx*4 + i] = 0.0;
+			}
+			globaldata[idx].xpos_nbhs = 0;
+			globaldata[idx].xneg_nbhs = 0;
+			globaldata[idx].ypos_nbhs = 0;
+			globaldata[idx].yneg_nbhs = 0;
+			globaldata[idx].entropy = 0.0;
+			globaldata[idx].delta = 0.0;
+			for(int i=0; i<20; i++)
+			{
+				xpos_conn[idx*20 + i] = 0;
+				xneg_conn[idx*20 + i] = 0;
+				ypos_conn[idx*20 + i] = 0;
+				yneg_conn[idx*20 + i] = 0; 
+			}
+		}
+		cout<<"\n-----End Read-----\n";
+	#endif
 
-#if 0
+	/* Read File Quadtree */
 	for(int idx=0; idx<numPoints; idx++)
 	{
-
-		int connectivity[20] = {0};
-		for(int iter=8; iter<result_doub[idx].size(); iter++)
-		{
-			connectivity[iter-8] = result_doub[idx][iter];
-		}
-
-		// Assign values to each of the elements of global data
-		// Make sure the types specifications are adhered to
-
-		globaldata[idx].localID = idx;
-		globaldata[idx].x = result_doub[idx][0];
-		globaldata[idx].y = result_doub[idx][1];
-		globaldata[idx].left = (int)result_doub[idx][2];
-		globaldata[idx].right = (int)result_doub[idx][3];
-		globaldata[idx].flag_1 = (int)result_doub[idx][4];
-		globaldata[idx].flag_2 = (int)result_doub[idx][5];
-		globaldata[idx].short_distance = result_doub[idx][6];
-		globaldata[idx].nbhs = (int)result_doub[idx][7];
-
-		for(int i=0; i<20; i++)
-		{
-			connec[idx*20 +i] = connectivity[i];
-			// (connectivity[i]!=0) cout<<"\n non-zero connectivity \n";
-		}
-
-
-		globaldata[idx].nx = 0.0;
-		globaldata[idx].ny = 0.0;
-
-		for(int i=0; i<4; i++)
-		{
-			prim[idx*4 + i] = defprimal[i];
-			flux_res[idx*4 + i] = 0.0;
-			q[idx*4 + i] = 0.0;
-			dq1[idx*4 + i] = 0.0;
-			dq2[idx*4 + i] = 0.0;
-			max_q[idx*4 + i] = 0.0;
-			min_q[idx*4 + i] = 0.0;
-			prim_old[idx*4 + i] = 0.0;
-		}
-
-		globaldata[idx].xpos_nbhs = 0;
-		globaldata[idx].xneg_nbhs = 0;
-		globaldata[idx].ypos_nbhs = 0;
-		globaldata[idx].yneg_nbhs = 0;
-
-		globaldata[idx].entropy = 0.0;
-		globaldata[idx].delta = 0.0;
-
-
-		for(int i=0; i<20; i++)
-		{
-			xpos_conn[idx*20 + i] = 0;
-			xneg_conn[idx*20 + i] = 0;
-			ypos_conn[idx*20 + i] = 0;
-			yneg_conn[idx*20 + i] = 0; 
-		}
-
-	}
-
-	cout<<"\n-----End Read-----\n";
-
-#endif
-
-/* Read File Quadtree */
-
-	for(int idx=0; idx<numPoints; idx++)
-	{
-
 		int connectivity[20] = {0};
 		for(int iter=11; iter<result_doub[idx].size(); iter++)
 		{
 			connectivity[iter-11] = result_doub[idx][iter];
 		}
-
-		// Assign values to each of the elements of global data
-		// Make sure the types specifications are adhered to
-
+ 	/* Assign values to each of the elements of global data and ensure the type specifications are adhered to */
 		globaldata[idx].localID = idx;
 		globaldata[idx].x = result_doub[idx][0];
 		globaldata[idx].y = result_doub[idx][1];
@@ -234,17 +222,12 @@ void meshfree_solver(char* file_name, int max_iters)
 		globaldata[idx].flag_2 = (int)result_doub[idx][5];
 		globaldata[idx].short_distance = result_doub[idx][9];
 		globaldata[idx].nbhs = (int)result_doub[idx][10];
-
 		for(int i=0; i<20; i++)
 		{
 			connec[idx*20 +i] = connectivity[i];
-			// (connectivity[i]!=0) cout<<"\n non-zero connectivity \n";
 		}
-
-
 		globaldata[idx].nx = result_doub[idx][6];
 		globaldata[idx].ny = result_doub[idx][7];
-
 		for(int i=0; i<4; i++)
 		{
 			prim[idx*4 + i] = defprimal[i];
@@ -256,16 +239,12 @@ void meshfree_solver(char* file_name, int max_iters)
 			min_q[idx*4 + i] = 0.0;
 			prim_old[idx*4 + i] = 0.0;
 		}
-
 		globaldata[idx].xpos_nbhs = 0;
 		globaldata[idx].xneg_nbhs = 0;
 		globaldata[idx].ypos_nbhs = 0;
 		globaldata[idx].yneg_nbhs = 0;
-
 		globaldata[idx].entropy = 0.0;
 		globaldata[idx].delta = 0.0;
-
-
 		for(int i=0; i<20; i++)
 		{
 			xpos_conn[idx*20 + i] = 0;
@@ -273,12 +252,11 @@ void meshfree_solver(char* file_name, int max_iters)
 			ypos_conn[idx*20 + i] = 0;
 			yneg_conn[idx*20 + i] = 0; 
 		}
-
 	}
-
 	cout<<"\n-----End Read-----\n";
+
 	if(configData.core.restart != 1) std::vector<vec_doub>().swap(result_doub);
-	
+
 	if(configData.core.restart == 1)
     {
 		// Check if any of the memory deallocations with delete[] new_file, or std::vector<vec_doub>().swap(result_doub) etc. are causing issues for the restart version
@@ -286,18 +264,14 @@ void meshfree_solver(char* file_name, int max_iters)
         std::fstream datafile(file_name, ios::in);
         std::string temp;
         std::regex ws_re("\\s+"); 
-
         int numPoints, max_iters;
         double residue;
-
         if(datafile.is_open())
-        {
-            
+        {      
             getline(datafile, temp);
             std::vector<std::string> temp_vec{std::sregex_token_iterator(temp.begin(), temp.end(), ws_re, -1), {}};
             numPoints = (int) std::stod(temp_vec[1]);
         }
-
         std::string new_file[numPoints];
         if(datafile.is_open())
         {
@@ -307,17 +281,13 @@ void meshfree_solver(char* file_name, int max_iters)
             }
         }
         datafile.close();
-
         std::vector<vec_str> result;
-        for(int i=0; i<numPoints; i++)  // This might include the first line as well so, you need to store that separately or just throw the 1st line away (the one that contains the file length)
-                                        // There are 48738 lines apart from that
+        for(int i=0; i<numPoints; i++)  
         { 
             std::vector<std::string> temp{std::sregex_token_iterator(new_file[i].begin(), new_file[i].end(), ws_re, -1), {}};
             result.push_back(temp);
         }
-
         std::vector<vec_doub> result_doub;
-
         for(int j=0; j<numPoints; j++)
         {
             vec_doub temp;
@@ -325,40 +295,29 @@ void meshfree_solver(char* file_name, int max_iters)
                 temp.push_back(std::stod(result[j][i]));
             result_doub.push_back(temp);
         }
-
         for(int i=0; i<numPoints; i++)
         {
         	for(int j=0; j<4; j++)
         		prim[i*4 + j] = result_doub[i][5+j];
         }
-    }
-
-	// Interior, Out and Wall were defined as Int64 in Julia, so am defining them as long long
-
+	}
+	
 	long long interior = configData.point_config.interior;
 	long long wall = configData.point_config.wall;
 	long long outer = configData.point_config.outer;
 
-
 	cout<<"\n-----Computing Normals-----\n";
-
 	for(int idx=0; idx<numPoints; idx++)
 		placeNormals(globaldata, idx, configData, interior, wall, outer);
 
 	cout<<"\n-----Start Connectivity Generation-----\n";
-
 	for(int idx=0; idx<numPoints; idx++)
 		calculateConnectivity(globaldata, idx, xpos_conn, xneg_conn, ypos_conn, yneg_conn, connec);
-
 	cout<<"\n-----Connectivity Generation Done-----\n";  
 
-	cout<<"\n"<<max_iters+1<<endl;
-
+	cout<<"\n"<<max_iters<<endl;
 	test_code(globaldata, configData, res_old, numPoints, max_iters, xpos_conn, xneg_conn, ypos_conn, yneg_conn, connec, prim, flux_res, q, dq1, dq2, max_q, min_q, prim_old); 
-
-
 	cout<<"\n--------Done--------\n"<<endl;
-
 }	
 
 
@@ -366,7 +325,6 @@ void run_code(Point* globaldata, Config configData, double res_old[1], int numPo
 	int* connec, double* prim, double* flux_res, double* q, double* dq1, double* dq2, double* max_q, double* min_q, double* prim_old)
 {
 	auto begin = std::chrono::high_resolution_clock::now();
-	// I'M ASSUMING IN FORTRAN ALSO THEY MEASURE THE COPY TIME AND NOT JUST COMPUTATION TIME. Just doesn't make sense to ignore copy time, although it won't be too huge
 	cudaStream_t stream;  
     Point* globaldata_d;
     unsigned int mem_size_A = sizeof(struct Point) * numPoints;
@@ -402,7 +360,7 @@ void run_code(Point* globaldata, Config configData, double res_old[1], int numPo
 
     double* res_sqr = new double[numPoints];
 
-    // Copy from host to device
+    /* Copy from host to device */
     checkCudaErrors(cudaMemcpyAsync(globaldata_d, globaldata, mem_size_A, cudaMemcpyHostToDevice, stream));
 	checkCudaErrors(cudaMemcpyAsync(tempdq_d, tempdq, mem_size_B, cudaMemcpyHostToDevice, stream));  
 	checkCudaErrors(cudaMemcpyAsync(xpos_conn_d, xpos_conn, mem_size_E, cudaMemcpyHostToDevice, stream));  
@@ -420,18 +378,21 @@ void run_code(Point* globaldata, Config configData, double res_old[1], int numPo
 	checkCudaErrors(cudaMemcpyAsync(prim_old_d, prim_old, mem_size_F, cudaMemcpyHostToDevice, stream));
 	
 	for (int i=0; i<max_iters; i++)
-	{
+	{	
+		/* The function inside which all the magic happens */
 		fpi_solver(i, globaldata_d, configData, res_old_d, res_sqr_d, numPoints, tempdq_d, stream, res_old, res_sqr, mem_size_C, mem_size_D, \
 			xpos_conn_d, xneg_conn_d, ypos_conn_d, yneg_conn_d, connec_d, prim_d, flux_res_d, q_d, dq1_d, dq2_d, max_q_d, min_q_d, prim_old_d); 
 
 	}
+
 	auto end = std::chrono::high_resolution_clock::now();
 	auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
 	printf("\nTime measured: %.5f seconds.\n", elapsed.count() * 1e-9);
-	// Copy from device to host, and free the device memory
+
+	/* Copy from device to host, and free the device memory */
 	checkCudaErrors(cudaMemcpyAsync(globaldata, globaldata_d, mem_size_A, cudaMemcpyDeviceToHost, stream));
 	checkCudaErrors(cudaMemcpyAsync(tempdq, tempdq_d, mem_size_B, cudaMemcpyDeviceToHost, stream));
-	// Do we wanna copy more stuff here? ...
+
     checkCudaErrors(cudaFree(globaldata_d)); 
     checkCudaErrors(cudaFree(tempdq_d));
     checkCudaErrors(cudaFree(res_old_d));
@@ -458,13 +419,12 @@ int* connec, double* prim, double* flux_res, double* q, double* dq1, double* dq2
 {
 	cout<<"\nStarting warmup function \n";
 	res_old[0] = 0.0;
-
 	cout<<"\nStarting main function \n";
 	TempqDers* tempdq = new TempqDers[numPoints];
-	
+	/* Set TempDq */
 	for (int i=0; i<numPoints; i++)
 		tempdq[i].setTempdq();
-
+	/* Run Meshfree Code */
 	run_code(globaldata, configData, res_old, numPoints, tempdq, max_iters, xpos_conn, xneg_conn, ypos_conn, yneg_conn, connec, prim, flux_res, q, dq1, dq2, max_q, min_q, prim_old); 
 
 }
