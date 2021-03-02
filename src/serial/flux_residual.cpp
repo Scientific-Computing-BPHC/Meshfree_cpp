1803d21d0f57298eca81aec1942690162487f214
Hari <harivallabha.rangarajan@gmail.com>
2021-03-02 07:15:40 +0530

    Add dq debug

--- src/serial/flux_residual.cpp
+++ src/serial/flux_residual.cpp
@@ -36,6 +36,20 @@ void cal_flux_residual(Point* globaldata, int numPoints, Config configData, int*
 void wallindices_flux_residual(Point* globaldata, int idx, double Gxp[4], double Gxn[4], double Gyp[4], double Gyn[4], Config configData, \
 	int* xpos_conn, int* xneg_conn, int* yneg_conn, double* flux_res, double* q, double* max_q, double* min_q, double* dq1, double* dq2)
 {
+	cout<<endl<<"Indic: "<<idx<<endl;
+	cout<<endl<<"Delta: "<<globaldata[idx].delta<<endl;
+	cout<<endl<<"Printing dq1, dq2: "<<endl;
+
+	for(int index = 0; index<4; index++)
+	{
+		cout<<std::fixed<<std::setprecision(17)<<dq1[idx*4 + index]<<"   ";
+	}
+
+	for(int index = 0; index<4; index++)
+	{
+		cout<<std::fixed<<std::setprecision(17)<<dq2[idx*4 + index]<<"   ";
+	}
+
 
 	wall_dGx_pos(globaldata, idx, Gxp,  configData, xpos_conn, q, max_q, min_q, dq1, dq2);
 	wall_dGx_neg(globaldata, idx, Gxn,  configData, xneg_conn, q, max_q, min_q, dq1, dq2);
@@ -45,8 +59,6 @@ void wallindices_flux_residual(Point* globaldata, int idx, double Gxp[4], double
 	for(int i=0; i<4; i++)
 		flux_res[idx*4 + i] = globaldata[idx].delta * (Gxp[i] + Gxn[i] + Gyn[i]) * 2;
 	
-	cout<<endl<<"Indic: "<<idx<<endl;
-	cout<<endl<<"Delta: "<<globaldata[idx].delta<<endl;
 	cout<<endl<<"Printing Gs: "<<endl;
 	for(int index = 0; index<4; index++)
 	{
